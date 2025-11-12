886c9a1cf693cf9d9649ffe5ab06bb97df103f10
Rosa Azminsdotter <rosa.azminsdotter@gmail.com>
2025-11-13 00:41:35 +0100

    Lägg till Projekt 7

---
+++ projekt-7/project.py
@@ -0,0 +1,146 @@
+import csv
+import random
+import matplotlib.pyplot as plt
+
+highScore = []
+     
+# Huvudmenyn som presenterar och hanterar de olika spelalternativen
+def menu():
+    while True:
+        rubrik = "Start Menu"
+        print(f"{rubrik:^20}")
+        print(f"{'-'*20}") 
+        print("1. Play")
+        print("2. Show scores")
+        print("3. Show diagram")
+        print("4. Quit")
+        print("PICK A NUMBER: ")
+        choice = input()
+
+        if choice == '1':
+            play_game()
+        elif choice == '2':
+            print_scores()
+        elif choice == '3':
+            diagram()
+        elif choice == '4':
+            print("Tack för ditt deltagande. Dina resultat har sparats i resultatfilen")
+            break
+        else:
+            print('Input not valid')
+
+# Den första funktionen som kallas på och som hämtar in värdena från CSV-filen till highScore-listan samt kallar på meny-funktionen
+def start():
+    import_scores()
+    menu()
+
+# En spel-funktion som kör självaste spelet och laddar upp nya resultat i CSV-filen samt lägger in dem i highScore-listan
+def play_game():
+    score = 0
+    doors = list(range(1,11))
+    treasure = random.randint(1, 10)
+
+    print('Pick a door')
+
+    while True:
+        for door in doors:
+            print(door)
+        guess = input()
+        
+        if guess == 'Quit' or guess == 'quit':
+           break
+
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("Only numbers allowed")
+            continue
+        
+        if guess not in doors:
+            print(f'Door {guess} is not an option')
+        elif guess == treasure:
+            score += 1
+            print(f'You found the treasure behind door {treasure} after {score} tries!')
+            name = input('Enter name: ')
+            print(f'{name}, your score is: {score}')
+
+            file = open('skattresultat.csv','a', newline='') 
+            csv_reader = csv.writer(file, delimiter = ',')
+            csv_reader.writerow([name, score])           #Lägger in resultatet i CSV - filen
+            highScore.append([name, score])              #Lägger in resultatet i highScore så man slipper imortera CSV-listan under denna körning
+            break
+        else:
+            doors.remove(guess)
+            score += 1
+            print('Try again')
+
+# Denna funktion importerar värdena från CSV-filen, eller skapar en fil om den inte redan finns
+def import_scores():
+    try:
+        file = open('skattresultat.csv','r')    
+        csv_reader = csv.reader(file, delimiter = ',')
+        for rad in csv_reader:
+            highScore.append(rad)
+        file.close()
+    except:
+        highScore.append(["Name", "Tries"])
+        file = open('skattresultat.csv','w', newline='')    
+        csv_writer = csv.writer(file, delimiter = ',')
+        for rad in highScore:
+            csv_writer.writerow(rad)
+        file.close()
+
+# En funktion som sorterar och skriver ut poänglistan till skärmen
+def print_scores():
+    sortedScores = sorted(highScore[1:], key=lambda x: int(x[1]))
+    title = highScore[0]
+    print(f"{title[0]:>8}{title[1]:>8}")
+    print(f"{'-'*20}")
+    
+    length = len(sortedScores)
+    i = 0
+
+    while i < length:
+        score = sortedScores[i]
+        if len(score) >= 2: # Detta ifall formatet hade blivit fel, att score inte har [0] och [1], eller om listan highScore är tom så består CSV-filen enbart av en tom []
+            print(f"{score[0]:>8}{score[1]:>8}", '\n')
+        i += 1
+
+# En funktion som visar ett diagram på listan    
+def diagram():
+    scoreList = []
+    frequencyList = []
+
+    x = range(1, 11)
+
+    # En while-loop som får fram en lista med bara poängen utan namn
+    i = 1
+    while i < len(highScore):
+        score = highScore[i]
+        if len(score) >= 2: 
+            scoreList.append(score[1])
+        i += 1
+
+    #En while-loop som får fram en lista med hur många gånger spelare har fått ett visst poäng
+    i = 1
+    while i < 11:
+        index = 0
+        amount = 0
+        while index < len(scoreList):
+            if int(scoreList[index]) == i:
+                amount += 1
+            index += 1
+        frequencyList.append(amount)
+        i += 1
+
+    plt.title('Score diagram')
+    plt.bar(x, frequencyList)
+    plt.xlabel('Score')
+    plt.ylabel('Frequency')
+    plt.yticks(range(0, max(frequencyList) + 3))
+    plt.xticks(range(0, 11))
+
+    plt.show()
+
+start()
+
---
+++ projekt-7/test.py
@@ -0,0 +1,170 @@
+import csv
+import random
+import matplotlib.pyplot as plt
+
+highScore = []
+     
+# Huvudmenyn som presenterar och hanterar de olika spelalternativen
+def menu():
+    while True:
+        rubrik = "Start Menu"
+        print(f"{rubrik:^20}")
+        print(f"{'-'*20}") 
+        print("1. Play")
+        print("2. Show scores")
+        print("3. Show diagram")
+        print("4. Quit")
+        print("PICK A NUMBER: ")
+        choice = input()
+
+        if choice == '1':
+            play_game()
+        elif choice == '2':
+            print_scores()
+        elif choice == '3':
+            diagram()
+        elif choice == '4':
+            print("Tack för ditt deltagande. Dina resultat har sparats i resultatfilen")
+            break
+        else:
+            print('Input not valid')
+
+# Den första funktionen som kallas på och som hämtar in värdena från CSV-filen samt kallar på meny-funktionen
+def start():
+    menu()
+
+# En spel-funktion som kör självaste spelet
+def play_game():
+    score = 0
+    doors = list(range(1,11))
+    treasure = random.randint(1, 10)
+
+    print('Pick a door')
+
+    while True:
+        for door in doors:
+            print(door)
+        guess = input()
+        
+        if guess == 'Quit' or guess == 'quit':
+           break
+
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("Only numbers allowed")
+            continue
+        
+        if guess not in doors:
+            print(f'Door {guess} is not an option')
+        elif guess == treasure:
+            score += 1
+            print(f'You found the treasure behind door {treasure} after {score} tries!')
+            name = input('Enter name: ')
+            print(f'{name}, your score is: {score}')
+            write_to_csv(name, score) #LÄGGER IN SCORES DIREKT MED APPEND
+            break
+        else:
+            doors.remove(guess)
+            score += 1
+            print('Try again')
+
+# Denna funktion importerar värdena från CSV-filen, eller skapar en fil om den inte redan finns
+#SORTERA HÄR IST
+def import_scores():
+    try:
+        file = open('skattresultat.csv','r')    
+        csv_reader = csv.reader(file, delimiter = ',')
+        for rad in csv_reader:
+            if len(highScore) > 0:
+                add_scores(rad[0],rad[1])
+            else:
+                highScore.append(['Name', 'Tries'])
+
+        file.close()
+        print(highScore) #DEBUG
+
+    except:
+        file = open('skattresultat.csv','w')  
+        csv_writer = csv.writer(file, delimiter=',')
+        csv_writer.writerow(['Name', 'Tries'])
+        highScore.append(['Name', 'Tries'])
+        print(highScore) #DEBUG
+
+# En funktion som lägger in alla rader i highScore-listan i en sorterad ordning
+def add_scores(name, score):     
+    sort = False
+    i = 1
+    new = [name, score]
+    length = len(highScore)
+    while not sort and i <= length:
+        if i == length:             # antingen så är filen tom, eller så har man nått slutet av listan
+            highScore.append(new)
+            sort = True
+        else:
+            old = highScore[i]
+            if new[1] > int(old[1]): # konverterar till int då import från csv blir str
+                i += 1
+            else:
+                highScore.insert(i, new)
+                sort = True
+
+# En funktion som laddar upp listan highScore till CSV-filen
+def write_to_csv(name, score):
+    import_scores()
+    file = open('skattresultat.csv','a', newline ='')
+    csv_writer = csv.writer(file, delimiter = ',')
+    csv_writer.writerow([name,score])
+
+# En funktion som skriver ut poänglistan till skärmen
+def print_scores():
+    import_scores()
+    title = highScore[0]
+    print(f"{title[0]:>8}{title[1]:>8}")
+    print(f"{'-'*20}")
+    length = len(highScore)
+    i = 1
+
+    while i < length:
+        score = highScore[i]
+        if len(score) >= 2: # Detta ifall formatet hade blivit fel, att score inte har [0] och [1], eller om listan highScore är tom så består CSV-filen enbart av en tom []
+            print(f"{score[0]:>8}{score[1]:>8}", '\n')
+        i += 1
+
+# En funktion som visar ett diagram på listan    
+def diagram():
+    import_scores()
+    score_list = []
+    frequencyList = []
+
+    x = range(1, 11)
+
+    # En while-loop som får fram en lista med bara poängen utan namn
+    i = 1
+    while i < len(highScore):
+        score = highScore[i]
+        if len(score) >= 2: 
+            score_list.append(score[1])
+        i += 1
+
+    #En while-loop som får fram en lista med hur många gånger spelare har fått ett visst poäng
+    i = 1
+    while i < 11:
+        index = 0
+        amount = 0
+        while index < len(score_list):
+            if int(score_list[index]) == i:
+                amount += 1
+            index += 1
+        frequencyList.append(amount)
+        i += 1
+
+    plt.title('Score diagram')
+    plt.bar(x, frequencyList)
+    plt.xlabel('Score')
+    plt.ylabel('Frequency')
+    plt.ylim(0, max(frequencyList) + 2)
+    plt.show()
+
+start()
+
